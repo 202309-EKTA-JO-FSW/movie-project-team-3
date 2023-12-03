ca28dc2cd1613b92e2dbd2d6c0200a937cf832cd
Farah alsoqi <farahmanna97@gmail.com>
2023-12-03 18:27:05 +0300

    Footr-Done

---
+++ movie/src/component/Footer.js
@@ -0,0 +1,41 @@
+
+const Footer = () => {
+ return (
+    <footer className="footer">
+      <p>© 2022 Movie Re:Coded</p>
+      <p>Project done by :  Baraa Sidair, Farah Alsoqi, and Ammar KolKo</p>
+      <br></br>
+      <div>
+                <a href="https://github.com/Ammar-coder87" target="_blank" rel="noreferrer">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="LinkedIn Logo" width="25" />
+                {" "}
+                    Ammar KolKo</a>
+                <a href="https://github.com/FarahAlsoqi" target="_blank" rel="noreferrer">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="LinkedIn Logo" width="25" />
+                {" "}   
+                    Farah Alsoqi</a>
+                <a href="https://github.com/baraab" target="_blank" rel="noreferrer">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="LinkedIn Logo" width="25" />
+                {" "}
+                    Baraa Sidair</a>
+      </div>
+      <br></br>
+      <div>
+            <a href="https://www.linkedin.com/in/ammar-kolko-39755674/" target="_blank" rel="noreferrer" style={{color:"blue"}}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png " alt="LinkedIn Logo" width="20" height="20" />
+            {" "}
+            Ammar KolKo</a>
+            <a href="https://www.linkedin.com/in/farah-alsoqi0/" target="_blank" rel="noreferrer" style={{color:"blue"}}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png " alt="LinkedIn Logo" width="20" height="20" />
+            {" "}
+            Farah Alsoqi</a>
+            <a href="https://www.linkedin.com/in/yourteammateusername/" target="_blank" rel="noreferrer" style={{color:"blue"}}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png " alt="LinkedIn Logo" width="20" height="20" />
+            {" "}
+            Baraa Sidair</a>
+      </div>
+    </footer>
+ );
+};
+
+export default Footer;
