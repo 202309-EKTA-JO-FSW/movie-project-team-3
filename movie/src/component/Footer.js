1f77871e134e754da5b1962affa5045c7d5d9b7c
Farah alsoqi <farahmanna97@gmail.com>
2023-12-07 09:08:24 +0300

    Footer Done

--- movie/src/component/Footer.js
+++ movie/src/component/Footer.js
@@ -1,4 +1,22 @@
 
+const teams = [
+    {
+    id:1,
+    name:"Baraa Sidair",
+    github:"https://github.com/baraab",
+    linkedin:""},
+    {
+        id:2,
+        name:"Farah Alsoqi",
+        github:"https://github.com/FarahAlsoqi",
+        linkedin:"https://jo.linkedin.com/in/farah-alsoqi0"},
+        {
+            id:3,
+            name:"Ammar KolKo",
+            github:"https://github.com/Ammar-coder87",
+            linkedin:"https://www.linkedin.com/in/ammar-kolko-39755674/"},
+]
+
 const Footer = () => {
  return (
     <footer className="footer">
@@ -6,34 +24,26 @@ const Footer = () => {
       <p>Project done by :  Baraa Sidair, Farah Alsoqi, and Ammar KolKo</p>
       <br></br>
       <div>
-                <a href="https://github.com/Ammar-coder87" target="_blank" rel="noreferrer">
-                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="LinkedIn Logo" width="25" />
-                {" "}
-                    Ammar KolKo</a>
-                <a href="https://github.com/FarahAlsoqi" target="_blank" rel="noreferrer">
-                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="LinkedIn Logo" width="25" />
-                {" "}   
-                    Farah Alsoqi</a>
-                <a href="https://github.com/baraab" target="_blank" rel="noreferrer">
-                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="LinkedIn Logo" width="25" />
-                {" "}
-                    Baraa Sidair</a>
+
+        {
+            teams.map((team) =>(
+              <ul key={team.id} className="footerImage">
+
+                <a href={`${team.linkedin}`}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png " alt="LinkedIn Logo" width="20" height="20" style={{margin:"5px"}}/>
+                    {team.name}
+                </a> 
+
+                <a href={`${team.github}`}>
+                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github Logo" width="25" />
+                    {team.name}
+                </a>
+                
+              </ul>
+            ))
+        }
       </div>
       <br></br>
-      <div>
-            <a href="https://www.linkedin.com/in/ammar-kolko-39755674/" target="_blank" rel="noreferrer" style={{color:"blue"}}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png " alt="LinkedIn Logo" width="20" height="20" />
-            {" "}
-            Ammar KolKo</a>
-            <a href="https://www.linkedin.com/in/farah-alsoqi0/" target="_blank" rel="noreferrer" style={{color:"blue"}}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png " alt="LinkedIn Logo" width="20" height="20" />
-            {" "}
-            Farah Alsoqi</a>
-            <a href="https://www.linkedin.com/in/yourteammateusername/" target="_blank" rel="noreferrer" style={{color:"blue"}}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png " alt="LinkedIn Logo" width="20" height="20" />
-            {" "}
-            Baraa Sidair</a>
-      </div>
     </footer>
  );
 };
--- movie/src/pages/_app.js
+++ movie/src/pages/_app.js
@@ -1,5 +1,6 @@
 import Footer from '@/component/Footer'
 import '@/styles/globals.css'
+
 export default function App({ Component, pageProps }) {
   return (
   <>
