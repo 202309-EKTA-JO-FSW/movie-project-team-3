ccafa0319412450036fd1fd98cf926c719bac76d
Farah Alsoqi <Farahmanna97@gmail.com>
2024-04-14 21:43:02 +0300

    add movie of specific actor

--- movie/src/pages/actors/index.js
+++ movie/src/pages/actors/index.js
@@ -19,6 +19,7 @@ function Actors({ actors }) {
       <br/>
       <br/>
       <br/>
+
     <div className="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4">
     {actors.map((actor) => (
             
